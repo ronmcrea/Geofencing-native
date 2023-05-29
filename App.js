4708ae452cd1a5b21fbe27244e17c6c2c0580b6a
Ronald Daniel <81346861+ronmcrea@users.noreply.github.com>
2023-05-29 11:36:41 +0530

    Add existing project files prior to the push to GitHub.

---
+++ App.js
@@ -0,0 +1,48 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import GeoFencing from 'react-native-geo-fencing';
+import { StyleSheet, Text, View } from 'react-native';
+
+componentDidMount() {
+  const polygon = [
+    { lat: 3.1336599385978805, lng: 101.31866455078125 },
+    { lat: 3.3091633559540123, lng: 101.66198730468757 },
+    { lat: 3.091150714460597,  lng: 101.92977905273438 },
+    { lat: 2.7222113428196213, lng: 101.74850463867188 },
+    { lat: 2.7153526167685347, lng: 101.47933959960938 },
+    { lat: 3.1336599385978805, lng: 101.31866455078125 } // last point has to be same as first point
+  ];
+
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      let point = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      GeoFencing.containsLocation(point, polygon)
+        .then(() => console.log('point is within polygon'))
+        .catch(() => console.log('point is NOT within polygon'))
+    },
+    (error) => alert(error.message),
+    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+  );
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
